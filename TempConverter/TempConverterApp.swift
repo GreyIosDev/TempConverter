d1d1dc44db053e119beb1d6c94a0c2714511d9e6
NNADI HENRY UGONNA <greyios845@gmail.com>
2023-05-01 10:03:05 +0300

    Initial Commit

---
+++ TempConverter/TempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterApp.swift
+//  TempConverter
+//
+//  Created by Grey  on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
