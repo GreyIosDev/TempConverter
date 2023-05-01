c3bf29df5133df0a382152a2a6f7ae84af783f9e
NNADI HENRY UGONNA <greyios845@gmail.com>
2023-05-01 11:56:53 +0300

    Temp converter full app

--- TempConverter/ContentView.swift
+++ TempConverter/ContentView.swift
@@ -8,19 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit = "Fahrenheit"
+    @State private var outputUnit = "Fahrenheit"
+    @State private var amount: Double = 0
+    @FocusState private var amountTextFieldIsFocused: Bool
+    
+    let units = ["Celsius": UnitTemperature.celsius,"Fahrenheit": UnitTemperature.fahrenheit, "Kelvin": UnitTemperature.kelvin]
+    
+    var result: Double {
+        guard
+            let inputUnit = units[inputUnit],
+            let outputUnit = units[outputUnit]
+        else { return 0 }
+        let input = Measurement (value: amount, unit: inputUnit)
+        let output = input.converted(to: outputUnit)
+        return output.value
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Picker ("Input Unit", selection: $inputUnit) {
+                        ForEach (Array (units.keys), id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }header: {
+                    Text("Input Unit" )
+                }
+                Section {
+                    Picker("Output Unit", selection: $outputUnit) {
+                        ForEach (Array (units.keys), id: \.self) { unit in
+                            Text(unit)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text ("Output Unit")
+                }
+                
+                Section {
+                    TextField("Amount", value: $amount, format: .number)
+                        .keyboardType (.decimalPad)
+                        .focused ($amountTextFieldIsFocused)
+                } header: {
+                    Text ("Amount")
+                }
+                Section {
+                    Text(result, format: .number)
+                }header: {
+                    Text ("Result")
+                }
+                
+            }
+            .navigationTitle("Temperature Converter")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done"){
+                        amountTextFieldIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+    
